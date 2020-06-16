d7855a5fe0503cf679b4d9a7dca23697a661ef7b
jvk <60311804+jvk78@users.noreply.github.com>
2020-06-16 17:26:53 +0600

    Add files via upload

--- assets/app/content_slides.js
+++ assets/app/content_slides.js
@@ -1,26 +1,22 @@
-console.log(123)
+//console.log(123)
 
 const leftProd = document.getElementById('left_slider')
 const rightProd = document.getElementById('right_slider')
 
 
 function createSlider(base, slider) {
-let SlideHTML = '';
-  
-  base.forEach(function({id, url, name, alt}) {
-    console.log(id)
-    
+  let SlideHTML = '';
+  base.forEach(function(elem) {
+//    console.log(id)
+
     SlideHTML += `<div class="products__group">
-            <div class="products__img">
-              <img class="products__discripts-img" src="${url}" alt="${alt}">
-            </div>
-            <div class="products__text text__white" id="${id}">${name}</div>
-          </div>`
-    
+                    <div class="products__img">
+                      <img class="products__discripts-img" src="${elem.url}" alt="${elem.alt}">
+                    </div>
+                    <div class="products__text text__white" id="${elem.id}">${elem.name}</div>
+                  </div>`
   })
-slider.innerHTML = SlideHTML;
+  slider.innerHTML = SlideHTML;
 }
 createSlider(prodLeft, leftProd)
 createSlider(prodRight, rightProd)
-
-console.log(SlideHTML)
